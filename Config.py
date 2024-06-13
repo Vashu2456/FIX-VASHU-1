d42e7d06be62c4604e3db62cdad5987f76b288c3
SKD-VASHU <165390858+Vashu2456@users.noreply.github.com>
2024-06-13 06:25:30 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -1,13 +1,13 @@
 import os
 
 class Config(object):
-    API_ID = int(os.environ.get("APP_ID", "6435225"))
-    API_HASH = os.environ.get("API_HASH", "4e984ea35f854762dcde906dce426c2d")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-    STRING_SESSION = os.environ.get("STRING_SESSION", "")
+    API_ID = int(os.environ.get("APP_ID", "25581940"))
+    API_HASH = os.environ.get("API_HASH", "1e80acd755ceab02b3e2a22d49b25c18")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7148375527:AAGn7ctUxdL-hM_YJUiTmFrc18zGEnpYAIA")
+    STRING_SESSION = os.environ.get("STRING_SESSION", "BQHAwxwAgsWyJTtsFXYr-RjZOA_pg-xtVwioD8s1lDs1d4W1gKTxToMQyeWBrsDmW1sr7TWj5aXGOFdhn9VwiYlMKGsfr9JuWZwy6sz_KkI2Vk6hS5u2Vnnowpy-_XHqqi7PCMZSBb0IEj03VJdpoKnKNqYa3rKbT_3sdmjHzNUTQyL-haralF2P8GfA4JLscfHNxgv9u2erNr8o6_LUOqf0F9lyUkmMsIL9oWNQC7oh4pXIwsdETNmgvGmZs6f6-RfZkdox6d3CgEEo3EbF2LFVfM1AmGc2riTnXPn7ZGqnLwtie2w5Lg0jMJf4lyMu8jrTxnGOqPblTpQC9oLbnoZ6gIk_KAAAAAGVW2_yAA")
     MANAGEMENT_MODE = os.environ.get("MANAGEMENT_MODE", True)
     HEROKU_MODE = os.environ.get("HEROKU_MODE", None)
-    BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
+    BOT_USERNAME = os.environ.get("BOT_USERNAME", "@FIXER_X_BOT")
     SUPPORT = os.environ.get("SUPPORT", "TheSupportChat") # Your Support
     CHANNEL = os.environ.get("CHANNEL", "TheUpdatesChannel") # Your Channel
     START_IMG = os.environ.get("START_IMG", "https://telegra.ph/file/3d8ecee0ba7dddfc6fce4.jpg")
